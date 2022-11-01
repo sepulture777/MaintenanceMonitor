19d6cf6eac146824918f7a37f39875d3c6c95a4e
Erich <ic19b071@technikum-wien.at>
2022-11-01 01:05:14 +0100

    Initial commit

---
+++ src/main/java/com/slm/mainmon/MainmonApplication.java
@@ -0,0 +1,13 @@
+package com.slm.mainmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainmonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainmonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slm/mainmon/MainmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slm.mainmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainmonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
